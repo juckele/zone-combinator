b504e7a3e74e61c83b8549376226a8a9a709da49
Davis Cook <daviscook447@gmail.com>
2023-10-19 22:20:51 -0400

    remainder of signals

--- control.lua
+++ control.lua
@@ -1,3 +1,8 @@
+function table_contains(table, check)
+  for k,v in pairs(table) do if v == check then return true end end
+  return false
+end
+
 ---Sets the signals on the zone combinator to the given values
 ---@param entity LuaEntity the combinator
 function set_zone_combinator_signals(entity, params)
@@ -5,20 +10,36 @@ function set_zone_combinator_signals(entity, params)
 
   local control_behavior_params = {}
   local index = 1
+  if params.index and params.signal then
+    table.insert(control_behavior_params, {index=index, signal={type="virtual", name=params.signal}, count=params.index})
+    index = index + 1
+  end
   if params.radius then
     table.insert(control_behavior_params, {index=index, signal={type="virtual", name="se-radius"}, count=params.radius})
     index = index + 1
   end
   if params.ticks_per_day then
-    table.insert(control_behavior_params, {index=index, signal={type="virtual", name="se-star"}, count=params.ticks_per_day})
+    table.insert(control_behavior_params, {index=index, signal={type="item", name="solar-panel"}, count=params.ticks_per_day})
     index = index + 1
   end
-  if params.hostiles_present then
-    table.insert(control_behavior_params, {index=index, signal={type="item", name="artillery-targeting-remote"}, count=params.hostiles_present})
+  if params.threat then
+    table.insert(control_behavior_params, {index=index, signal={type="item", name="artillery-targeting-remote"}, count=params.threat})
     index = index + 1
   end
-  if params.has_water then
-    table.insert(control_behavior_params, {index=index, signal={type="fluid", name="water"}, count=params.has_water})
+  if not params.waterless then
+    table.insert(control_behavior_params, {index=index, signal={type="fluid", name="water"}, count=1})
+    index = index + 1
+  end
+  if params.biter_meteors then
+    table.insert(control_behavior_params, {index=index, signal={type="virtual", name="se-meteor"}, count=1})
+    index = index + 1
+  end
+  if params.plagued then
+    table.insert(control_behavior_params, {index=index, signal={type="item", name="se-plague-bomb"}, count=1})
+    index = index + 1
+  end
+  if params.life_support then
+    table.insert(control_behavior_params, {index=index, signal={type="item", name="se-lifesupport-canister"}, count=1})
     index = index + 1
   end
   if params.primary_resource then
@@ -39,38 +60,47 @@ function set_zone_combinator_signals(entity, params)
   control_behavior.parameters = control_behavior_params
 end
 
+local icon_prefix = "virtual-signal/"
+
+function get_zone_signal(surface_index)
+  local zone = remote.call("space-exploration", "get_zone_from_surface_index", {surface_index = surface_index})
+  if not zone then return nil, nil end
+
+  local zone_icon = remote.call("space-exploration", "get_zone_icon", {zone_index = zone.index})
+
+  return zone.index, string.sub(zone_icon, string.len(icon_prefix) + 1, string.len(zone_icon))
+end
+
 function update_zone_combinator(entity)
-  -- the player does not need to change the values in this combinator
-  entity.operable = false
-    
   -- compute and set the values for the combinator
   local surface_index = entity.surface.index
   local zone = remote.call("space-exploration", "get_zone_from_surface_index", {surface_index = surface_index})
   if not zone then return end
 
-  local hostiles_present = true
-  if zone.hostiles_extict then
-    hostiles_present = false
-  end
-
-  local has_water = true
-  if zone.tags then
-    for _, tag in pairs(zone.tags) do
-      if tag == "water_none" then
-        has_water = false
-      end
-    end
-  end
-
+  local threat = remote.call("space-exploration", "threat_for_surface", {surface_index = surface_index})
   local robot_attrition = remote.call("space-exploration", "robot_attrition_for_surface", {surface_index = surface_index})
+  local hazards = remote.call("space-exploration", "hazards_for_surface", {surface_index = surface_index})
+  local index, signal = get_zone_signal(surface_index)
+  local plagued = table_contains(hazards, "plague-world")
+  local life_support = plagued or (zone.type ~= "moon" and zone.type ~= "planet") -- this probably will change in some future version of SE where some planets/moons don't just happen to all have breathable atmospheres
+  local waterless = table_contains(hazards, "waterless") or (zone.type ~= "moon" and zone.type ~= "planet")
+  local primary_resource = nil
+  if zone.type ~= "orbit" then
+    primary_resource = zone.primary_resource
+  end
 
   set_zone_combinator_signals(entity, {
     radius = zone.radius,
     ticks_per_day = zone.ticks_per_day,
-    hostiles_present = hostiles_present and 1,
-    has_water = has_water and 1,
-    primary_resource = zone.primary_resource,
+    threat = math.floor(threat * 100),
+    waterless = waterless,
+    biter_meteors = table_contains(hazards, "biter-meteors"),
+    plagued = plagued,
+    primary_resource = primary_resource,
     robot_attrition = math.floor(robot_attrition * 100),
+    index = index,
+    signal = signal,
+    life_support = life_support,
   })
 end
 
